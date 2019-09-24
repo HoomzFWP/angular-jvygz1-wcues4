226780de244f4863f9fec41d44b4425fbc3cc5e2
HoomzFWP <ndamte@gmail.com>
2019-09-23 22:27:22 -0500

    fix cart component issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -17,9 +18,9 @@ import { ShippingComponent } from './shipping/shipping.component';
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
+      { path: '', component: CartComponent },
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
+      { path: '', component: ProductDetailsComponent },
       { path: 'shipping', component: ShippingComponent },
     ])
   ],
